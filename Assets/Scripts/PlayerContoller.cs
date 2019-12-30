c573bf35492c63d5c37f7af7fa7cad8f4b5baae4
Proinfinto <furkanyaran@hotmail.com>
2019-12-30 09:22:50 +0300

    First
    First project upload

---
+++ Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+
+    public Animator MyAnimator;
+
+    public static PlayerContoller instance;
+
+    public string areaTransitionName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")) * moveSpeed;
+
+        MyAnimator.SetFloat("moveX", theRB.velocity.x);
+        MyAnimator.SetFloat("moveY", theRB.velocity.y);
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            MyAnimator.SetFloat("lastMoveX",Input.GetAxisRaw("Horizontal"));
+            MyAnimator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+    }
+}
